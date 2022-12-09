fabc28f51d8059a2a5eb98764f65a0f4f394fac6
Falcowoski <leonardo.falcoski@gmail.com>
2022-12-09 01:27:32 -0300

    feat: Adicionado o plugin 'Dialog'

--- quasar.config.js
+++ quasar.config.js
@@ -104,7 +104,9 @@ module.exports = configure((ctx) => ({
     // directives: [],
 
     // Quasar plugins
-    plugins: [],
+    plugins: [
+      'Dialog',
+    ],
   },
 
   // animations: 'all', // --- includes all animations
