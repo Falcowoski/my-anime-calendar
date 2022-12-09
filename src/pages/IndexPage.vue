86225de3392747e8e94e6d3d2dd80f2f5ab14f55
Falcowoski <leonardo.falcoski@gmail.com>
2022-12-09 01:29:55 -0300

    refactor: Ajustada a página inicial

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,24 @@
 <template>
-  <q-page class="flex flex-center">
-    <h2>É os guri!</h2>
+  <q-page class="container">
+    <AnimeCard
+      v-for="anime in animes"
+      :key="anime.id"
+      :id="anime.id"
+      :title="anime.title"
+      :broadcast="anime.broadcast"
+      :picture="anime.main_picture.large"
+      :added="anime.added"
+      :handleClick="() => handleClick(anime.id)"
+    />
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import AnimeCard from 'src/components/AnimeCard.vue';
 
 export default defineComponent({
   name: 'IndexPage',
-
   data() {
     return {
       animes: null,
@@ -20,10 +29,50 @@ export default defineComponent({
     this.getAnimes();
   },
 
-  methods: {
-    getAnimes() {
-      this.animes = localStorage.getItem('anime_season');
+  watch: {
+    animes: {
+      handler() {
+        this.setAnimes();
+      },
+      deep: true,
     },
   },
+
+  methods: {
+    getAnimes() {
+      this.animes = JSON.parse(localStorage.getItem('anime_season'));
+    },
+
+    setAnimes() {
+      localStorage.setItem('anime_season', JSON.stringify(this.animes));
+    },
+
+    handleClick(id) {
+      // Get index
+      const index = this.animes.findIndex((anime) => anime.id === id);
+
+      // Get object
+      const anime = this.animes[index];
+
+      // Invert the value
+      anime.added = !anime.added;
+
+      // Update state
+      this.animes[index] = anime;
+    },
+  },
+
+  components: { AnimeCard },
 });
 </script>
+
+<style lang="scss">
+.container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 172px);
+  justify-content: center;
+  gap: 1rem;
+  padding: 18px 14px;
+}
+
+</style>
