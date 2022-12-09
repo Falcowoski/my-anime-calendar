f781199c348dd79d2d59e0d22bc60d8f5e0962f2
Leonardo Falcoski <85199880+Falcowoski@users.noreply.github.com>
2022-12-09 15:12:07 -0300

    fix: Corrigida condicional

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,13 @@ export default defineComponent({
 
   created() {
     // Skip this step if already has data in the Local Storage
-    if (!localStorage.getItem('anime_season')) {
+    if (localStorage.getItem('anime_season') !== null) {
+      console.warn('Already has data in Local Storage. Skipping loading.');
       return;
     }
 
     // Save the current anime season in the Client Local Storage
+    console.warn('Saving data in the Local Storage.');
     const animes = Object.values(animeSeason.data).map((anime) => anime.node);
     localStorage.setItem('anime_season', JSON.stringify(animes));
   },
