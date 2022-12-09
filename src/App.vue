8432ea1a1784a519a71839262f51826236debbac
Falcowoski <leonardo.falcoski@gmail.com>
2022-12-08 21:27:18 -0300

    chore: Create Quasar Aplication

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import animeSeason from './assets/data/anime_season_2022_fall.json';
+
+export default defineComponent({
+  name: 'App',
+
+  created() {
+    // Save the current anime season in the Client Local Storage
+    localStorage.setItem('anime_season', JSON.stringify(animeSeason));
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="flex flex-center">
+    <h2>É os guri!</h2>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+
+  data() {
+    return {
+      animes: null,
+    };
+  },
+
+  created() {
+    this.getAnimes();
+  },
+
+  methods: {
+    getAnimes() {
+      this.animes = localStorage.getItem('anime_season');
+    },
+  },
+});
+</script>
